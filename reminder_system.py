d6afdf05c9f08074835dea6d6045e34ac5f39221
AyeshaBanuTappal <24691A0544@mits.ac.in>
2026-04-05 12:42:49 +0530

    Update reminder_system.py

--- reminder_system.py
+++ reminder_system.py
@@ -23,7 +23,7 @@ for r in reminders:
     print("Target:", r["time"])
     print("Diff:", diff)
 
-    if 0 <= diff <= 60:
+    if -300 <= diff <= 300:
         try:
             print("Trying to send email...")
 
